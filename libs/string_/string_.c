8ccaee717690aac0ae105419c8eb07297bca453f
RuSkeleton <yaroslaw.roe@yandex.ru>
2024-04-13 14:01:23 +0300

    String fing func

--- libs/string_/string_.c
+++ libs/string_/string_.c
@@ -20,6 +20,34 @@ char* find(char *begin, char *end, int ch) {
     return begin;
 }
 
+char* findNonSpace(char *begin) {
+    while (isspace(*begin) && *begin != '\0') {
+        begin++;
+    }
+    return begin;
+}
+
+char* findSpace(char *begin) {
+    while (!isspace(*begin) && *begin != '\0') {
+        begin++;
+    }
+    return begin;
+}
+
+char *findNonSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin >= rend && isspace(*rbegin)) {
+        rbegin--;
+    }
+    return rbegin;
+}
+
+char *findSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin >= rend && !isspace(*rbegin)) {
+        rbegin--;
+    }
+    return rbegin;
+}
+
 void test_find_1() {
     char string[] = "hello";
 
